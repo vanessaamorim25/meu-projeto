12e5ff0aa756d55c21272ef1c6ec57e7444c91e4
Vanessa <vra.reboucas@gmail.com>
2026-06-21 19:22:40 -0400

    1924m

--- calculadora.test.js
+++ calculadora.test.js
@@ -4,7 +4,7 @@ const soma = require('./calculadora');
 describe('Calculadora', () => {
 
     it('Deve somar dois números', () => {
-        assert.equal(soma(2,3),6);
+        assert.equal(soma(2,3),5);
     });
 
 });
